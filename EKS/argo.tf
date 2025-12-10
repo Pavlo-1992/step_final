215f45a2fe9f1ccd1de277cb6ed8fbf8ab0e5b4b
pavlo-1992 <savanchukpavel@gmail.com>
2025-12-10 19:03:51 +0200

    add

---
+++ EKS/argo.tf
@@ -0,0 +1,36 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "5.51.6"
+
+  set {
+    name  = "server.ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "server.ingress.ingressClassName"
+    value = "nginx"
+  }
+
+  set {
+    name  = "server.ingress.hosts[0]"
+    value = "argocd.${var.name}.devops9.test-danit.com"
+  }
+
+  set {
+    name  = "server.ingress.https"
+    value = "false"
+  }
+
+  set {
+    name  = "server.extraArgs[0]"
+    value = "--insecure"
+  }
+
+  depends_on = [module.eks, helm_release.nginx_ingress]
+}
