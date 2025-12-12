e07df22f9d44431886411d6c49cb11aa8e2662ec
Pavlo-1992 <savanchukpavel@gmail.com>
2025-12-12 09:57:33 +0200

    Update EKS worker node scaling configuration

--- eks+argo/eks-worker-nodes.tf
+++ eks+argo/eks-worker-nodes.tf
@@ -5,9 +5,9 @@ resource "aws_eks_node_group" "danit" {
   subnet_ids = var.subnets_ids
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 1
-    min_size     = 1
+    min_size     = 3
   }
 
   instance_types = ["t3.medium"]
