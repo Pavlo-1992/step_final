15d7fcfb7f06f6624f8406c3ee0def89bb913593
Pavlo-1992 <savanchukpavel@gmail.com>
2025-12-13 13:30:32 +0200

    Remove ingress_nginx_hostname output
    Removed output for ingress_nginx_hostname from outputs.tf

--- eks+argo/outputs.tf
+++ eks+argo/outputs.tf
@@ -12,9 +12,3 @@ output "argocd_url" {
   description = "ArgoCD ingress URL"
   value       = "http://${local.argocd_domain}"
 }
-
-output "ingress_nginx_hostname" {
-  value = data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname
-  description = "Public hostname of the ingress-nginx LoadBalancer"
-}
-
